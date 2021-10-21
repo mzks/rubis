9a17e1c10dca086020aad13c6fb9ab813d9680b1
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2021-10-21 16:57:56 +0900

    Add config generator

--- rubis/cli.py
+++ rubis/cli.py
@@ -4,11 +4,14 @@ from glob import glob
 import pkg_resources
 from rubis.core import run
 
+
 def main():
 
     parser = argparse.ArgumentParser(description='Rubis Control tool')
     parser.add_argument('-c', '--config', default='default_config',
                         help='Config json filename with path')
+    parser.add_argument('-g', '--generate_config', action='store_true',
+                        help='Generate config example')
     parser.add_argument('-p', '--path', default='default',
                         help='File output path')
     parser.add_argument('-f', '--file_header', default='default')
@@ -17,6 +20,11 @@ def main():
                         help='Time interval to collect data', type=int)
 
     args = parser.parse_args()
+
+    if args.generate_config:
+        config_filename = pkg_resources.resource_filename('rubis','data') + '/default_config.json'
+        shutil.copyfile(config_filename, './custom_config.json')
+        return
 
     if args.config == 'default_config':
         config_filename = pkg_resources.resource_filename('rubis','data') + '/default_config.json'
