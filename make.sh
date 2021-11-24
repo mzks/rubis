f2f5d471c1ae1556eb658b29226fcb90e5af301c
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2021-11-24 16:36:44 +0100

    jump to version 0.7

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ python3 setup.py bdist_wheel
 
 echo Done.
 echo local install
-echo 'python3 -m pip install dist/rubis-0.6.1-py3-none-any.whl --upgrade'
+echo 'python3 -m pip install dist/rubis-0.7.0-py3-none-any.whl --upgrade'
 
 echo upload pypi
 echo 'python3 -m twine upload --repository pypi dist/*'
