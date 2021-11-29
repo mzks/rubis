d75654af1915343e3b87e018b2762a5d4f321027
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2021-11-29 15:13:11 +0100

    Release v1.0.0

--- rubis/cli.py
+++ rubis/cli.py
@@ -39,7 +39,7 @@ def main():
     parser.add_argument('-v', '--version', action='store_true')
 
     args = parser.parse_args()
-    version = '0.9.0'
+    version = '1.0.0'
     if args.version:
         print('rubis version : ' + version)
         return
